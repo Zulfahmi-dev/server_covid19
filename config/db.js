e4e1c41d7691957201cf11e3b7440e7e94a9edf6
Zulfahmi-dev <fzul115@gmail.com>
2020-04-19 22:08:50 +0700

    Update connection string database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'covid19',
-    password: 'root',
-    port: 5432,
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
 })
+
+// untuk lokal
+// const db = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'covid19',
+//     password: 'root',
+//     port: 5432,
+// })
 
 module.exports = db;
