2d7cee54476d9e8340b4648b49b11a3437d919a6
Zulfahmi-dev <fzul115@gmail.com>
2020-04-19 23:06:19 +0700

    deploy to heroku

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get('/', (req, res)=>{
-    res.send({hai:"hello"})
-})
 router(app);
 
 // production
